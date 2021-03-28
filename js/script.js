90ed1523c79810923d5ec8d543ffd0a4cddf5ff7
DevWarIt <ioel.mm3395@outlook.com>
2021-03-28 14:10:30 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     OverlayScrollbars(document.querySelectorAll("body"), {});
     ScrollReveal().reveal('img', {delay: 100, container: '.os-viewport'});
-    ScrollReveal().reveal('.services_desc', {delay: 100, container: '.os-viewport'});
+    ScrollReveal().reveal('.services_desc', {delay: 100,reset: true, container: '.os-viewport'});
     ScrollReveal().reveal('.desc', {delay: 100, reset: true, interval: 200, container: '.os-viewport'});
 });
